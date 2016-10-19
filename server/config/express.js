e585cf17bc17fc844e58c9b0c7283e4f4790d065
amanmahalgavan <amahalgavan@hrgdev.com>
2016-10-19 18:34:12 +0530

    changes in gamelogic

--- server/config/express.js
+++ server/config/express.js
@@ -46,8 +46,9 @@ module.exports = function(app) {
   
   if ('production' === env) {
     app.use(favicon(path.join(config.root, 'public', 'favicon.ico')));
-    app.use(express.static(path.join(config.root, 'public')));
-    app.set('appPath', path.join(config.root, 'public'));
+    // app.use(express.static(path.join(config.root, 'public')));
+    app.use(express.static(path.join(config.root, 'client')));
+    app.set('appPath', path.join(config.root, 'client'));
     app.use(morgan('dev'));
   }
 
@@ -55,6 +56,7 @@ module.exports = function(app) {
     app.use(require('connect-livereload')());
     app.use(express.static(path.join(config.root, '.tmp')));
     app.use(express.static(path.join(config.root, 'client')));
+    app.use('/assets', express.static(process.cwd() + '/client/assets'));
     app.set('appPath', path.join(config.root, 'client'));
     app.use(morgan('dev'));
     app.use(errorHandler()); // Error handler - has to be last
